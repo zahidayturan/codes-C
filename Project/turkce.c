5cb159126a51448956b7a5968431bb3c60e6fd56
Zahid Ayturan <21060607@stu.omu.edu.tr>
2022-04-02 22:15:14 +0300

    add

---
+++ Project/turkce.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// ./getchar <text.txt
+int main(void) 
+{
+    int c, i=0, nwhites, nother, nturkce;
+    int ndigit[10];
+
+    nwhites = nother = 0;
+    for (i=0; i < 10; i++)
+        ndigit[i] = 0;
+
+    while ((c = getchar()) != EOF)
+        if (c >= '0' && c <= '9')
+            ++ndigit[c - '0'];
+        else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhites;
+        else
+            ++nother;
+
+        for (i=0; i < 10; i++)
+            printf(" %d %s%d\n",i,"Number of: ",ndigit[i]);
+            printf(" White space = %d, Other = %d\n",
+            nwhites, nother);
+
+        {
+    return 0;
+}
+
+// ı, ğ, ü, ş, ö, ç, Ğ, Ü, Ş, İ, Ö, Ç
