8e25d44f97a6534151880d9eb423cb023bb04698
Zahid Ayturan <21060607@stu.omu.edu.tr>
2022-04-29 17:03:00 +0300

    add

---
+++ MathProg/fact.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int sayi,i,sonuc=1;
+	
+	printf("Faktoriyel alinmasini istediginiz sayi : ");
+	scanf("%d",&sayi);
+
+	for(int i=2; i<=sayi ; i++)     
+		sonuc = sonuc * i; 
+
+	printf("%d sayisinin faktoriyeli : %d\n", sayi,sonuc); 
+    return 0;
+}
