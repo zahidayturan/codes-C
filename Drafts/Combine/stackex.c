7a9d62e6f4558cda115b8bd6a5d6cec250e34383
Zahid Ayturan <21060607@stu.omu.edu.tr>
2022-07-10 12:07:56 +0300

    gl

---
+++ Drafts/Combine/stackex.c
@@ -0,0 +1,51 @@
+//trial
+
+#include <stdio.h>
+
+#define MAXITEM 100 /* maximum depth of stack */
+
+static int sp;                /* next free stack position */
+static double stack[MAXITEM]; /* stack (of double number values) */
+
+/* push: push f onto value stack */
+void push(double f)
+{
+        if (sp < MAXITEM)
+                stack[sp++] = f;
+        else
+                printf("Error: Stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+        if (sp > 0)
+                return stack[--sp];
+        else {
+                printf("Error: Stack empty\n");
+                return 0.0;
+        }
+}
+
+/* dump: print stack items */
+void dump(void)
+{
+        if (sp > 0) {
+                int i = sp;
+
+                while (i-- > 0)
+                        printf("%d: %f\n", i, stack[i]);
+        } else {
+                printf("Stack empty!\n");
+                return;
+        }
+}
+
+/* flush: reset stack while printing all the items */
+void flush(void)
+{
+        while (--sp >= 0)
+                printf("%d: %f\n", sp, stack[sp]);
+}
+
+//cc -o stack getnum.c stack.c main.c && ./stack
---
+++ Drafts/Pointer/deneme.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+for (int i =0; i<=10 ; ++i)
+printf("%d",++i);
+return 0;
+}
+
+
+
+
+
+/*
+int main(){
+int dizi[5] ;
+
+  dizi[0] = 27;
+  dizi[1] = 348;
+  dizi[2] = 51;
+  dizi[2]++;
+  dizi[3] = 1254;
+  dizi[4] = 127;
+printf("%d ", dizi[2]);
+return 0;
+}
+*/
