6d6e9a8a56a2cffc380750097e8d41149e2a3091
Zahid Ayturan <91957947+zahidayturan@users.noreply.github.com>
2023-01-10 17:49:32 +0300

    Update splaytree.c

--- DataStructers/trees/splaytree.c
+++ DataStructers/trees/splaytree.c
@@ -36,69 +36,61 @@ struct node *splay(struct node *root, int key)
 	if (root == NULL || root->key == key)
 		return root;
 
-	// Key lies in left subtree
+	
 	if (root->key > key)
 	{
-		// Key is not in tree, we are done
+		
 		if (root->left == NULL) return root;
 
 		// Zig-Zig (Left Left)
 		if (root->left->key > key)
 		{
-			// First recursively bring the key as root of left-left
+			
 			root->left->left = splay(root->left->left, key);
 
-			// Do first rotation for root, second rotation is done after else
+			
 			root = rightRotate(root);
 		}
 		else if (root->left->key < key) // Zig-Zag (Left Right)
 		{
-			// First recursively bring the key as root of left-right
+			
 			root->left->right = splay(root->left->right, key);
 
-			// Do first rotation for root->left
+			
 			if (root->left->right != NULL)
 				root->left = leftRotate(root->left);
 		}
 
-		// Do second rotation for root
+		
 		return (root->left == NULL)? root: rightRotate(root);
 	}
-	else // Key lies in right subtree
+	else 
 	{
-		// Key is not in tree, we are done
+		
 		if (root->right == NULL) return root;
 
 		// Zag-Zig (Right Left)
 		if (root->right->key > key)
 		{
-			// Bring the key as root of right-left
 			root->right->left = splay(root->right->left, key);
 
-			// Do first rotation for root->right
 			if (root->right->left != NULL)
 				root->right = rightRotate(root->right);
 		}
 		else if (root->right->key < key)// Zag-Zag (Right Right)
 		{
-			// Bring the key as root of right-right and do first rotation
 			root->right->right = splay(root->right->right, key);
 			root = leftRotate(root);
 		}
 
-		// Do second rotation for root
 		return (root->right == NULL)? root: leftRotate(root);
 	}
 }
 
-// The search function for Splay tree. Note that this function
-// returns the new root of Splay Tree. If key is present in tree
-// then, it is moved to root.
 struct node *search(struct node *root, int key)
 {
 	return splay(root, key);
 }
-
 
 void preOrder(struct node *root)
 {
